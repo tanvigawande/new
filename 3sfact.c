06bbc9d417e2090845a0a16070e459d4aab6c8d6
tanvigawande <140898479+tanvigawande@users.noreply.github.com>
2023-07-29 18:58:31 +0530

    Add files via upload

---
+++ 3sfact.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+ {
+   int n;
+   int  i=1;
+  int sum=1;
+  int fact=1;
+   printf("enter the number");
+   scanf("%d",&n);
+   while(i<=n)
+   {  
+     fact=fact*1;
+     sum=sum+1/fact;
+printf("%d",sum);
+      
+      i++;
+       }
+         return 0;
+         }
+      
+         
